2b37ecf28cd9fae9d75da0a4b098795b56c4470d
Macheza Dzabala <dzabalamacheza@gmail.com>
2020-11-04 14:08:06 +0200

    Implement Search UI
    - Added Search UI
    - Added Logout button under menu
    
    N/A

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:idove/utilities/Colors.dart';
 import 'package:idove/utilities/TextStyles.dart';
 
-Widget wideButton({@required Function onPressed, @required String buttonText}) {
+Widget wideButton(
+    {@required Function onPressed,
+    @required String buttonText,
+    Color color = PRIMARY_COLOR}) {
   return SizedBox(
     width: double.infinity * 0.80,
     child: RaisedButton(
+      color: color,
       onPressed: onPressed,
       child: Padding(
         padding: const EdgeInsets.all(17.0),
